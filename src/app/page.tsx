967ba2e65c262ce0c03101c742493f6113dd6678
peter <peterphongpak@gmail.com>
2026-02-09 19:18:03 +0700

    chore: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <h1 className="text-4xl font-semibold">Hello Oonjai!</h1>
+    </div>
+  );
+}
