e7825d53187411540eda9b04aa7c0db618d62ace
yam <sachhu12004@gmail.com>
2026-03-13 15:52:47 +0700

    feat: implement responsive how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/ui/landing/Navbar";
 import ProblemSection from "@/components/ui/landing/ProblemSection";
 import FeaturesSection from "@/components/ui/landing/FeaturesSection";
 import { Feature } from "framer-motion";
+import HowItWorksSection from "@/components/ui/landing/HowItWorksSection";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <ProblemSection />
       <FeaturesSection />
+      <HowItWorksSection />
     </>
   );
 }
