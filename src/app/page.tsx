e211599720f5eda75469d6a2e4410e2ae0bf2a7e
yam <sachhu12004@gmail.com>
2026-03-13 15:40:39 +0700

    feat: implement responsive hero and problem sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import HeroSection from "@/components/ui/landing/HeroSection";
 import Navbar from "@/components/ui/landing/Navbar";
+import ProblemSection from "@/components/ui/landing/ProblemSection";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HeroSection />
+      <ProblemSection />
       <div className="min-h-screen w-full flex items-center justify-center">
         <h1 className="text-4xl font-semibold">Hello Oonjai hi!</h1>
         <h2 className="text-oonjai-blue-400 font-medium text-4xl">Text</h2>
--- src/components/ui/landing/Navbar.tsx
+++ src/components/ui/landing/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 w-full bg-lightCream/90 backdrop-blur-md border-b border-lightGrey/30">
+    <nav className="sticky top-0 z-50 w-full bg-PrimaryCream backdrop-blur-md border-b border-lightGrey/30">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 md:h-20">
           
